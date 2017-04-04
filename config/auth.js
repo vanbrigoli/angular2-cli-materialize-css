92d243735efb3297cb70d203fafebe8aaa5fd907
brigoli <ecyt9ARG4383>
2017-04-04 20:16:02 +0800

    change callback to heroku

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1721944211430977', // your App ID
         'clientSecret'  : '02224e72ea3c42bc5919c65b2a2ff769', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURLDev'   : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL': 'https://accrefy.herokuapp.com/auth/facebook/callback'
     }
 }
