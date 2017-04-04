dea4fad2ff4a86baa5cc0ff746e48e20c1f9cd95
brigoli <ecyt9ARG4383>
2017-04-04 21:11:20 +0800

    use accrefy callback

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
         'clientID'      : '1721944211430977', // your App ID
         'clientSecret'  : '02224e72ea3c42bc5919c65b2a2ff769', // your App Secret
         'callbackURLDev'   : 'http://localhost:3000/auth/facebook/callback',
-        'callbackURL': 'https://accrefy.herokuapp.com/auth/facebook/callback'
+        'callbackURL': 'https://accrefy.com/auth/facebook/callback'
     }
 }
