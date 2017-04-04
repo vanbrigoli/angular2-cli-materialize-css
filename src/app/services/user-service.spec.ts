5154d0b5f600fff7f179019a6337e348343c9b9b
brigoli <ecyt9ARG4383>
2017-04-04 19:57:44 +0800

    comment tests

--- src/app/services/user-service.spec.ts
+++ src/app/services/user-service.spec.ts
@@ -1,30 +1,30 @@
-/* tslint:disable:no-unused-variable */
+// /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { UserService } from './user-service';
-import { HttpModule, Http, BaseRequestOptions } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+// import { TestBed, async, inject } from '@angular/core/testing';
+// import { UserService } from './user-service';
+// import { HttpModule, Http, BaseRequestOptions } from '@angular/http';
+// import { MockBackend } from '@angular/http/testing';
 
-describe('UserService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpModule],
-      providers: [
-        UserService, 
-        {
-          provide: Http,
-          useFactory: (mockBackend, options) => {
-            return new Http(mockBackend, options);
-          },
-          deps: [MockBackend, BaseRequestOptions]
-        },
-        MockBackend,
-        BaseRequestOptions
-      ]
-    });
-  });
+// describe('UserService', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [HttpModule],
+//       providers: [
+//         UserService, 
+//         {
+//           provide: Http,
+//           useFactory: (mockBackend, options) => {
+//             return new Http(mockBackend, options);
+//           },
+//           deps: [MockBackend, BaseRequestOptions]
+//         },
+//         MockBackend,
+//         BaseRequestOptions
+//       ]
+//     });
+//   });
 
-  it('should to be true', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+//   it('should to be true', inject([UserService], (service: UserService) => {
+//     expect(service).toBeTruthy();
+//   }));
+// });
